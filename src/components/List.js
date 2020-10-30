12731f3c00308512287ddc8de758a86d1628d178
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-30 22:48:57 +0100

    :bug: Fix error with undefined currentToDos.map

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,6 @@
 import ListElement from "./ListElement";
 
-export default function List({ currentToDos }) {
-  //   const items = ["blaa", "blupp", "blii"];
-  //   const listItems = items.map((item) => <li key={item.toString()}>{item}</li>);
-
+export default function List({ currentToDos = [] }) {
   return (
     <ul data-testid="list">
       {currentToDos.map((todo) => (
