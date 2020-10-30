6e0fb96e522b160a40d1aac0f60ca0ad7a27dc2c
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-10-30 14:58:35 +0100

    Add onSubmit event

--- src/components/FormElement.js
+++ src/components/FormElement.js
@@ -2,9 +2,16 @@ import React from "react";
 import "./FormElement.css";
 
 export default function FormElement() {
+  let inputValue = null;
   return (
-    <form>
-      <input type="text" placeholder="Add Item" />
+    <form
+      onSubmit={(event) => {
+        inputValue = document.getElementById("input").value;
+        event.preventDefault();
+        alert(inputValue);
+      }}
+    >
+      <input type="text" placeholder="Add Item" id="input" />
       <button>Add</button>
     </form>
   );
