8d225e87bb80017944ff8e369fb66f3c19602d17
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-10-31 23:06:11 +0100

    Add edit button to every list item

--- src/components/ListElement.js
+++ src/components/ListElement.js
@@ -1,3 +1,10 @@
+import EditButton from "./EditButton";
+
 export default function ListElement({ todo }) {
-  return <li data-testid="listElement">{todo}</li>;
+  return (
+    <li data-testid="listElement">
+      {todo}
+      <EditButton />
+    </li>
+  );
 }
