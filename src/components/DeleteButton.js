481acc9bb4d717a275985379b18d0ce1b7d929d1
Naasgul <lnaas@hotmail.de>
2020-10-31 13:23:14 +0100

    Add delete button without functionality

---
+++ src/components/DeleteButton.js
@@ -0,0 +1,3 @@
+export default function DeleteElement() {
+  return <button data-testid="listElement">Delete</button>;
+}
--- src/components/ListElement.js
+++ src/components/ListElement.js
@@ -1,3 +1,10 @@
+import DeleteButton from "./DeleteButton";
+
 export default function ListElement({ todo }) {
-  return <li data-testid="listElement">{todo}</li>;
+  return (
+    <div>
+      <li data-testid="listElement">{todo}</li>
+      <DeleteButton />
+    </div>
+  );
 }
