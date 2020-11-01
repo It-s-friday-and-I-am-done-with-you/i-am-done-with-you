326b17f9acf3fb034c8f354244b563f9705603d9
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-11-01 19:09:15 +0100

    :sparkles: Add input form

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form>
+      <input type="text" className="input-text" />
+      <button className="button-add" type="submit">
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default Form;
