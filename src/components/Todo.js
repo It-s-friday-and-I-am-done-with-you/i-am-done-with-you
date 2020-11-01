bdcb953ab3b5d2c8d1b36a7e5442747e0849749c
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-11-01 23:23:29 +0100

    :sparkles: Add functionality for done and delete todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,34 @@
 import React from "react";
 import "./Todo.css";
 
-const Todo = ({ text }) => {
+const Todo = ({ text, todo, todos, setTodos }) => {
+  const deleteHandler = () => {
+    setTodos(todos.filter((item) => item.id !== todo.id));
+  };
+  const completeHandler = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        }
+        return item;
+      })
+    );
+  };
   return (
     <div className="todo">
-      <li className="todo-item">{text}</li>
-      <button className="btn btn-done">Done</button>
-      <button className="btn btn-delete">Delete</button>
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+        {text}
+      </li>
+      <button onClick={completeHandler} className="btn btn-done">
+        Done
+      </button>
+      <button onClick={deleteHandler} className="btn btn-delete">
+        Delete
+      </button>
     </div>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,18 @@ import React from "react";
 import "./TodoList.css";
 import Todo from "./Todo";
 
-const TodoList = ({ todos = [] }) => {
+const TodoList = ({ todos = [], setTodos }) => {
   return (
     <div className="todo-wrapper">
       <ul data-testid="todo-list" className="list">
         {todos.map((todo) => (
-          <Todo key={todo.id} text={todo.text} />
+          <Todo
+            key={todo.id}
+            text={todo.text}
+            todo={todo}
+            todos={todos}
+            setTodos={setTodos}
+          />
         ))}
       </ul>
     </div>
