eaeffee8a294ddc6c816007f291cfd517268fcfd
Matthias Heisterkamp <hello@mhstrkmp.dev>
2020-11-01 19:13:19 +0100

    :sparkles: Add selection for filtering

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,6 +7,13 @@ const Form = () => {
       <button className="button-add" type="submit">
         Add
       </button>
+      <div className="selection">
+        <select name="todos" className="filter">
+          <option value="all">All</option>
+          <option value="done">Done</option>
+          <option value="todo">ToDo</option>
+        </select>
+      </div>
     </form>
   );
 };
